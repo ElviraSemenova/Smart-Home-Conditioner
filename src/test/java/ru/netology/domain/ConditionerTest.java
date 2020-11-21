a677ef6ed3fac2ccea6e74a0bc0aa297a78910b9
Elvira Semenova <shalopayka@gmail.com>
2020-11-21 13:48:41 +0300

    initial commit

---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,49 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+    @Test
+    void shouldIncreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(20);
+        conditioner.increaseCurrentTemperature();
+        int expected = 21;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotIncreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(30);
+        conditioner.increaseCurrentTemperature();
+        int expected = 30;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldDecreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(25);
+        conditioner.decreaseCurrentTemperature();
+        int expected = 24;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotDecreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(10);
+        conditioner.decreaseCurrentTemperature();
+        int expected = 10;
+        int actual = conditioner.getCurrentTemperature();
+        assertEquals(expected, actual);
+    }
+}
